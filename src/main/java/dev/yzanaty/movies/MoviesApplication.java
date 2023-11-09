4e74befe6c03c6f737f628afb40e7ce80c1bbb00
Yassine Zanaty <yassinezanaty@Yassines-MacBook-Pro.local>
2023-11-09 13:19:10 +0100

    Initial commit

---
+++ src/main/java/dev/yzanaty/movies/MoviesApplication.java
@@ -0,0 +1,14 @@
+package dev.yzanaty.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dev/yzanaty/movies/Review.java
@@ -0,0 +1,23 @@
+package dev.yzanaty.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@Document(collection = "reviewIds")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+    private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
+}
